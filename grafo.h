f2f47e5030c61b716f1e1aaaa7015e0f019a6e52
rrivas <rrivas@utec.edu.pe>
2023-07-06 08:48:52 -0500

    update 6/7/2023 add_edges

--- grafo.h
+++ grafo.h
@@ -8,7 +8,11 @@
 #include <utility>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 #include <forward_list>
+#include <queue>
+#include <stack>
+
 using namespace std;
 
 template<
@@ -23,6 +27,16 @@ class grafo {
     unordered_map<KeyType, ItemType> vertices;
     // Lista de Adyacentes
     vector<forward_list<AdjacentType>> adjacent_vertices;
+    KeyType get_key(VertexType vertex) {
+        auto it = find_if(begin(vertices), end(vertices), [vertex](const auto& item){
+           return item.second.first == vertex;
+        });
+        // Validar si no lo encontro
+        if (it == end(vertices)) {
+            throw runtime_error("clave no encontrada");
+        }
+        return it->first;
+    }
 public:
     grafo() = default;
     // Adicionar Vertices
@@ -44,6 +58,63 @@ public:
         adjacent_vertices[vx1].emplace_front(vx2, weight);
         adjacent_vertices[vx2].emplace_front(vx1, weight);
     }
+    template <typename UnaryFunction>
+    void bfs(KeyType key, UnaryFunction fun) {
+        // Definir variables
+        queue<VertexType> q;
+        unordered_set<VertexType> visited;
+        auto is_visited = [&visited](auto vertex) { return visited.find(vertex) != end(visited); };
+        // Agregar el primer valor
+        auto fv = vertices[key].first;
+        q.push(fv);
+        visited.insert(fv);
+        // El Bucle
+        while (!q.empty()) {
+            // obtener el primer valor del queue
+            auto v = q.front();
+            fun(get_key(v));
+            q.pop();
+            // agregar los adyacentes al queue
+            for (auto [a, w]: adjacent_vertices[v]) {
+                if (!is_visited(a)) {
+                    q.push(a);
+                    visited.insert(a);
+                }
+            }
+        }
+    }
+    template <typename UnaryFunction>
+    void dfs(KeyType key, UnaryFunction fun) {
+        // Definir variables
+        stack<VertexType> s;
+        unordered_set<VertexType> visited;
+        // Tomar el primer valor
+        auto fv = vertices[key].first;
+        // Agregar al stack y set
+        s.push(fv);
+        visited.insert(fv);
+        // bucle
+        while(!s.empty()) {
+            auto v = s.top(); // Paso #3
+
+            // Buscar si hay algun adyacente no visita
+            auto av = adjacent_vertices[v];
+            auto it = find_if(begin(av), end(av), [&visited](auto adjacent) {
+                return visited.find(adjacent.first) == end(visited);
+            });
+
+            if (it != end(av)) {    // Paso #4
+                s.push(it->first);
+                visited.insert(it->first);
+            }
+            else {                  // Paso #5
+                s.pop();
+                fun(get_key(v));
+            }
+        }
+
+    }
+
 };
 
 #endif //PROG3_TE03_20231_GRAFOS_GRAFO_H
--- main.cpp
+++ main.cpp
@@ -10,11 +10,20 @@ int main() {
     g.add_vertex({"O", 4});
     g.add_vertex({"U", 5});
     // Aristas
+    g.add_edge("A", "I", 20);
     g.add_edge("A", "E", 10);
-    g.add_edge("A", "E", 10);
-    g.add_edge("A", "E", 10);
-    g.add_edge("A", "E", 10);
-    g.add_edge("A", "E", 10);
+    g.add_edge("E", "I", 15);
+    g.add_edge("E", "O", 7);
+    g.add_edge("O", "U", 12);
+
+    g.bfs("A", [](auto key) {
+        cout << key << " ";
+    });
+    cout << endl;
+    g.dfs("A", [](auto key) {
+        cout << key << " ";
+    });
+    cout << endl;
 
     return 0;
 }
